d58930d83fe1af88ae2cd9099ee05e153e074091
minsun <kor3334@naver.com>
2021-02-14 20:54:25 +0900

    게시물 detail 기능 추가

--- todo_board/views.py
+++ todo_board/views.py
@@ -12,6 +12,10 @@ class Todo_board(generic.TemplateView):
 
         return render(request, template_name, {"todo_list" : todo_list})
 
+class Todo_board_detail(generic.DetailView):
+    model = TodoList
+    template_name = "todo_board/todo_board_detail.html"
+    context_object_name = 'todo_list'
 
 def check_post(request):
     template_name = 'todo_board/todo_board_success.html'
@@ -29,21 +33,3 @@ def check_post(request):
         return render(request, template_name, {"form":form})
 
 
-# def check_post(request):
-#     template_name = 'todo_board/todo_board_success.html'
-#     if request.method == "POST":
-#         if str(request.path).split("/board/")[1].split("/")[0] == "insert":
-#             form = TodoForm(request.POST)
-#             if form.is_valid():
-#                 message = "일정을 추가하였습니다."
-#                 if len(request.POST.get('title')) < 2:
-#                     message = "제목은 2글자 이상으로 입력해주세요!"
-#                 else:
-#                     todo = form.save(commit=False)
-#                     todo.todo_save()
-#                 return render(request, template_name, {"message": message})
-
-#     else:
-#         template_name = 'todo_board/todo_board_insert.html'
-#         form = TodoForm
-#         return render(request, template_name, {"form" : form})
