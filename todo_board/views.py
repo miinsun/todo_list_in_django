ef95a3f54ba6a2f8f52c812a422c489bff562a22
minsun <kor3334@naver.com>
2021-02-14 21:11:12 +0900

    게시물 update 기능 추가

--- todo_board/views.py
+++ todo_board/views.py
@@ -17,6 +17,26 @@ class Todo_board_detail(generic.DetailView):
     template_name = "todo_board/todo_board_detail.html"
     context_object_name = 'todo_list'
 
+# update와 관련된 기능을 제공해줌. save 기능과 form 데이터를 받아오는 기능이 필요
+class Todo_board_update(generic.UpdateView):
+    model = TodoList
+    fields = ('title', 'content', 'end_date')
+    template_name = 'todo_board/todo_board_update.html'
+    success_url = '/board/'
+
+    # 만약 form이 유효하다면 form_valid 함수를 실행해서 폼을 저장
+    def form_valid(self, form):
+        form.save()
+        return render(self.request, 'todo_board/todo_board_success.html', {"message": "일정을 업데이트 했습니다"})
+
+    def get(self, request, *args, **kwargs):
+        #오브젝트를 받아와서 폼 클래스를 받아온 후 이것을 return
+        self.object = self.get_object()
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(object = self.object, form=form)
+        return self.render_to_response(context)
+
 def check_post(request):
     template_name = 'todo_board/todo_board_success.html'
     
