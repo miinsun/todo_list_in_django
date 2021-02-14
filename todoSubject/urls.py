eacf963e20fd0f3a3b1ff34125042db4c2a7724c
minsun <kor3334@naver.com>
2021-02-14 22:26:33 +0900

    Ajax 기능 추가 1. 체크박스 선택(마감유무 설정) 2. 선택된 일정을 마우스 드래그로 우선순위 지정

--- todoSubject/urls.py
+++ todoSubject/urls.py
@@ -20,8 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo_main.urls')),
-    path('index/', include('todo_main.urls')),
-    path('home/', include('todo_main.urls')),
 
     # board app
     path('board/', include('todo_board.urls')),
--- todo_board/models.py
+++ todo_board/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Max
 
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
